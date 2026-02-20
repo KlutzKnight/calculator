c3ec37ee48363ab3d2ca5ca118f2658381c685fa
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-02-20 19:38:42 +0530

    Fix some bugs and make code more robust

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if(Number(b) == 0)
+    let numA = Number(a);
+    let numB = Number(b);
+    if(numB === 0)
         return "Can't divide by zero";
-    return Number(a) / Number(b);
+    return Math.round((numA/numB) * 10000) / 10000;
 }
 
 function operate(ope1, ope2, opcode) {
@@ -36,14 +38,12 @@ function operate(ope1, ope2, opcode) {
 
 function isNumeric(str) {
     // Matches for only numeric
-    let regExp = /^[0-9]+$/;
-    return regExp.test(str);
+    return "0123456789.".includes(str);
 }
 
 function isOperation(str) {
     // Matches for only the basic operations
-    let regExp = /^[-+*÷]$/;
-    return regExp.test(str);
+    return "+-*÷".includes(str);
 }
 
 function clearDisplay() {
@@ -59,8 +59,15 @@ function calculate(expression) {
     let ope1 = "";
     let ope2 = "";
     let opcode = "";
+    let ope1Negative = false;
 
     let i = 0;
+    
+    // Check for negative sign at the start
+    if(expression[i] === "-") {
+        ope1Negative = true;
+        i++
+    }
     // Get operand 1
     while(i < expression.length && !isOperation(expression[i])) {
         ope1 += expression[i];
@@ -76,16 +83,19 @@ function calculate(expression) {
         i++;
     }
 
+    if(ope1Negative)
+        ope1 = -ope1;
+
     return operate(ope1, ope2, opcode);
 }
 
 function parse(node) {
-    if(node.tagName != "BUTTON")
+    if(node.tagName !== "BUTTON")
         return null;
 
     let keyPressed = node.textContent;
     // If C is pressed clear everything and return
-    if(keyPressed == "C") {
+    if(keyPressed === "C") {
         clearDisplay();
         clearExpression();
         return;
@@ -119,6 +129,7 @@ function parse(node) {
         else {
             alert(result);
             clearExpression();
+            clearDisplay();
         }
         clearDisplayFlag = true;
     }
