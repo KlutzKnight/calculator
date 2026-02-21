49ef784d2367eaae0b9195a353442396d823f069
Rafi Ahmad <215167856+KlutzKnight@users.noreply.github.com>
2026-02-21 13:02:30 +0530

    change architecture from string parser to calculator

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const calculator = document.querySelector("#calculator");
 const display = document.querySelector("#display");
-let clearDisplayFlag = true;
-let expression = "";
+
+let resetDisplay = true;
+let resultEvaluated = false;
+
+let result = 0;
+let operand = "";
+let operation = "+";
 
 function add(a, b) {
     return Number(a) + Number(b);
@@ -19,21 +24,18 @@ function divide(a, b) {
     let numA = Number(a);
     let numB = Number(b);
     if(numB === 0)
-        return "Can't divide by zero";
+        return null;
     return Math.round((numA/numB) * 10000) / 10000;
 }
 
 function operate(ope1, ope2, opcode) {
-    let result;
     switch(opcode) {
-        case "+": result = add(ope1, ope2); break;
-        case "-": result = subtract(ope1, ope2); break;
-        case "*": result = multiply(ope1, ope2); break;
-        case "÷": result = divide(ope1, ope2); break;
+        case "+": return add(ope1, ope2);
+        case "-": return subtract(ope1, ope2);
+        case "*": return multiply(ope1, ope2);
+        case "÷": return divide(ope1, ope2);
         default: return null;
     }
-
-    return result;
 }
 
 function isNumericChar(str) {
@@ -50,144 +52,66 @@ function clearDisplay() {
     display.value = "";   
 }
 
-function clearExpression() {
-    expression = "";
-}
-
-function calculate(expression) {
-    // expresssion is a string
-    let ope1 = "";
-    let ope2 = "";
-    let opcode = null;
-    let result = 0;
-    let ope1Negative = false;
-    let disableDecimalFlag = false;
-
-    let i = 0;
-    // Check for any operation code at the start and ignore them
-    while(i < expression.length && isOperation(expression[i])) {
-        i++;
-    }
-
-    // If - right before ope1, set the flag to negate it
-    if(expression[i-1] === "-") {
-        ope1Negative = true;
-    }
-
-    // Get operand 1
-    while(i < expression.length && !isOperation(expression[i])) {
-        // Ignore Multiple decimal points
-        if(expression[i] === ".") {
-            if(disableDecimalFlag) {
-                i++;
-                continue;
-            }
-            else disableDecimalFlag = true;
-        }
-        ope1 += expression[i];
-        i++;
-    }
-
-    // Get operation Code
-    while(i < expression.length && isOperation(expression[i])) {
-        opcode = expression[i];
-        i++;
-    }
-
-    disableDecimalFlag = false;
-    // Get operand 2
-    while(i < expression.length && (expression[i] !== "=") && !isOperation(expression[i])) {
-        // Ignore Multiple decimal points
-        if(expression[i] === ".") {
-            if(disableDecimalFlag) {
-                i++;
-                continue;
-            }
-            else disableDecimalFlag = true;
-        }
-        ope2 += expression[i];
-        i++;
-    }
-
-    if(ope1Negative)
-        ope1 = -ope1;
-
-    result = operate(ope1, ope2, opcode);
-    
-    while(i < expression.length && expression[i] !== "=") {
-        // Get operation Code
-        while(i < expression.length && isOperation(expression[i])) {
-            opcode = expression[i];
-            i++;
-        }
-        
-        disableDecimalFlag = false;
-        // Get operand 2
-        ope2 = "";
-        while(i < expression.length && (expression[i] !== "=") && !isOperation(expression[i])) {
-            // Ignore Multiple decimal points
-            if(expression[i] === ".") {
-                if(disableDecimalFlag) {
-                    i++;
-                    continue;
-                }
-                else disableDecimalFlag = true;
-            }
-            ope2 += expression[i];
-            i++;
-        }
-        result = operate(result, ope2, opcode);
-    }
-
-    return result;
+function clearState() {
+    result = 0;
+    operand = "";
+    operation = "+";
 }
 
 function parse(node) {
     if(node.tagName !== "BUTTON")
         return null;
 
-    let keyPressed = node.textContent;
-    // If C is pressed clear everything and return
-    if(keyPressed === "C") {
+    let buttonPressed = node.textContent;
+    // If C is pressed, clear everything and return
+    if(buttonPressed === "C") {
         clearDisplay();
-        clearExpression();
+        clearState();
         return;
     }
 
-    // Clear the display if the flag is set
-    if(clearDisplayFlag && isNumericChar(keyPressed)) {
+    if(resultEvaluated) {
+        if(isNumericChar(buttonPressed)) {
+            clearDisplay();
+            clearState();
+        }
+        resultEvaluated = false;
+    }
+
+    // Clear the display
+    if(resetDisplay) {
         clearDisplay();
-        clearDisplayFlag = false;
-        if(!isOperation(expression.at(-1)))
-            clearExpression();
+        resetDisplay = false;   
     }
 
-    // Display text on screen
-    if(isNumericChar(keyPressed)) {
-        display.value += keyPressed;
-    }
-    // Sets the clear display flag so it is cleared on next input
-    else if(isOperation(keyPressed)) {
-        clearDisplayFlag = true;
-    }
-
-    // Evaluate and Clear the expression and display
-    expression += keyPressed;
-    if(keyPressed === "=") {
-        let result = calculate(expression);
-        if(!isNaN(result)) {
-            display.value = result;
-            expression = display.value;
+    if(buttonPressed === "=") {
+        // Evaluate and display the result
+        result = operate(result, operand, operation);
+        if(result == null) {
+            display.value = "Can't Divide by 0";
         }
         else {
-            alert(result);
-            clearExpression();
-            clearDisplay();
+            display.value = result;
         }
-        clearDisplayFlag = true;
+        operand = "";
+        resultEvaluated = true;
+        return;
+    }
+
+
+    if(!isOperation(buttonPressed)) {
+        display.value += buttonPressed;
+        operand += buttonPressed;
+    }
+
+    if(isOperation(buttonPressed)) {
+        result = operate(result, operand, operation);
+        operation = buttonPressed;
+        operand = "";
+        resetDisplay = true;
     }
 }
 
 clearDisplay();
-clearExpression();
+clearState();
 calculator.addEventListener("click", e => parse(e.target));
